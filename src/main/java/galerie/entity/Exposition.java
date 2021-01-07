b052383be89cfe17fc059d2a4fe6f33989004d42
GeraldEscolano <67196459+GeraldEscolano@users.noreply.github.com>
2021-01-07 12:45:54 +0100

    galeri/expo/transac/tab done

---
+++ src/main/java/galerie/entity/Exposition.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galerie.entity;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+/**
+ *
+ * @author escol
+ */
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity // Une entité JPA
+public class Exposition {
+     @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Integer id;
+     
+    @Column(unique=true)
+    @NonNull
+    private Date date;
+    
+    @Column(unique=true)
+    @NonNull
+    private String intitule;
+    
+    @Column(unique=true)
+    @NonNull
+    private int duree;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "Exposition")
+    private List<Transaction> transactions;
+    
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "Exposition")
+    private List<Tableau> tableaux;
+
+    public Exposition(Integer id, Date date, String intitule, int duree, List<Transaction> transactions, List<Tableau> tableaux) {
+        this.id = id;
+        this.date = date;
+        this.intitule = intitule;
+        this.duree = duree;
+        this.transactions = transactions;
+        this.tableaux = tableaux;
+    }
+        
+    
+    
+    public float Ca(){
+    float ca = 0;
+     for (Transaction t : this.transactions){
+       ca = ca + t.getPrixVente();
+     }   
+     return ca;
+    }
+     
+     
+    }
--- src/main/java/galerie/entity/Galerie.java
+++ src/main/java/galerie/entity/Galerie.java
@@ -1,23 +1,67 @@
 package galerie.entity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.*;
 import lombok.*;
 
 // Un exemple d'entité
 // On utilise Lombok pour auto-générer getter / setter / toString...
 // cf. https://examples.javacodegeeks.com/spring-boot-with-lombok/
-@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
 @Entity // Une entité JPA
 public class Galerie {
-    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "galerie_id")
     private Integer id;
 
-    @Column(unique=true)
+    @Column(unique = true)
     @NonNull
     private String nom;
-    
-    @Column(unique=true)
+
+    @Column(unique = true)
     @NonNull
     private String adresse;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "Galerie")
+    private List<Exposition> expos;
+
+    public Galerie(Integer id, String nom, String adresse, List<Exposition> expos) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.expos = expos;
+    }
+
     
-    // TODO : Mettre en oeuvre la relation oneToMany vers Exposition
+    public float CAannuel(int a) {
+        float ca_annuel = 0;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy");
+        Date d;
+        try {
+            d = formatter.parse(String.valueOf(a));
+            for (Exposition e : expos) {
+                if (d.equals(e.getDate())) {
+                    ca_annuel = e.Ca() + ca_annuel;
+
+                }
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(Galerie.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return ca_annuel;
+    }
+    
+    
 }
